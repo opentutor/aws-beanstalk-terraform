3f524aecccf5a4d8821f2fbb9d1113e78487c312
larry kirschner <larrykirschner@gmail.com>
2021-04-12 10:56:15 -0700

    opentutor 2.0 infrastructure (#7)
    * requires vars for secret_jwt_secret and secret_api_secret
    
     * allow set version from tf
    
     * update module terraform-aws-vpc to 0.21.0 from 0.18.2
    
     * update module terraform-aws-dynamic-subnets to 0.37.6 from 0.36.0
    
     * update module terraform-aws-elastic-beanstalk-application.git  to 0.11.0 from 0.9.0
    
     * update module terraform-aws-elastic-beanstalk-environment to 0.37.0 from 0.34.0

--- template/main.tf
+++ template/main.tf
@@ -5,7 +5,8 @@ provider "aws" {
 module "opentutor_beanstalk_deployment" {
     # change the tag below as needed
     # or use source="./.." for local dev
-    source      = "git::https://github.com/opentutor/terraform-opentutor-aws-beanstalk?ref=tags/0.1.0"
+    # MAKE SURE USING LATEST VERSION/TAG IN YOURS
+    source      = "git::https://github.com/opentutor/aws-beanstalk-terraform?ref=tags/2.0.0"
     aws_acm_certificate_domain      = var.aws_acm_certificate_domain
     aws_availability_zones          = var.aws_availability_zones
     aws_region                      = var.aws_region
@@ -13,10 +14,87 @@ module "opentutor_beanstalk_deployment" {
     eb_env_env_vars                 = var.eb_env_env_vars
     eb_env_namespace                = var.eb_env_namespace
     eb_env_stage                    = var.eb_env_stage
+    eb_env_version_label            = var.eb_env_version_label
     google_client_id                = var.google_client_id
+    secret_api_secret                = var.secret_api_secret
+    secret_jwt_secret                = var.secret_jwt_secret
     secret_mongo_uri                = var.secret_mongo_uri
     site_domain_name                = var.site_domain_name
     vpc_cidr_block                  = var.vpc_cidr_block
+}
+
+variable "aws_acm_certificate_domain" {
+  type        = string
+  description = "domain name to find ssl certificate"
+}
+
+variable "aws_availability_zones" {
+  type        = list(string)
+  description = "List of availability zones"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "aws_route53_zone_name" {
+  type        = string
+  description = "name to find aws route53 zone, e.g. opentutor.info."
+}
+
+variable "eb_env_env_vars" {
+  type        = map(string)
+  default     = {}
+  description = "Map of custom ENV variables to be provided to the application running on Elastic Beanstalk, e.g. env_vars = { DB_USER = 'admin' DB_PASS = 'xxxxxx' }"
+}
+
+variable "eb_env_namespace" {
+  type        = string
+  description = "Namespace, which could be your organization name, e.g. 'eg' or 'cp'"
+}
+
+variable "eb_env_stage" {
+  type        = string
+  description = "Stage, e.g. 'prod', 'staging', 'dev', or 'test'"
+  default     = "test"
+}
+
+variable "eb_env_version_label" {
+  type        = string
+  description = "generally leave this blank, unless you're trying to force a different app version along with infra updates"
+  default     = ""
+}
+
+variable "google_client_id" {
+  type        = string
+  description = "google client id for google auth (https://developers.google.com/identity/one-tap/web/guides/get-google-api-clientid)"
+}
+
+variable "secret_api_secret" {
+  type        = string
+  description = "an arbitrary secret shared among services to allow admin access in inter-service graphql requests"
+}
+
+variable "secret_jwt_secret" {
+  type        = string
+  description = "an arbitrary secret shared among services to encode/decode jwt tokens"
+}
+
+variable "secret_mongo_uri" {
+  type        = string
+  description = "fully qualified mongo uri (includes user and password) for connections to a mongodb instance backend (presumably external, e.g. mongodb.com)"
+}
+
+variable "site_domain_name" {
+  type        = string
+  description = "the public domain name for this site, e.g. opentutor.yoursite.org"
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "cidr for the vpc, generally can leave the default unless there is conflict"
+  default     = "10.7.0.0/16"
 }
 
 output "efs_file_system_id" {
--- template/scret.auto.tfvars
+++ template/scret.auto.tfvars
@@ -3,4 +3,6 @@
 # Once it has secrets in it that you never
 # commit it or push to vc
 
+secret_api_secret = "<arbitrary api secret used to elevate privileges for inter-service graphql requests>"
+secret_jwt_secret = "<arbitrary secret used to encode/decode jwt tokens"
 secret_mongo_uri = "<mongo url w user, password, etc.>"
